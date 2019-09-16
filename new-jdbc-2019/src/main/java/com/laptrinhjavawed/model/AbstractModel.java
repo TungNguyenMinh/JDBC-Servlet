ec03d2a46dc94a2a0b9043a846bf16988bc06fa1
=lethigam150594 <=tungnguyenminh271294@gmail.com>
2019-09-16 19:02:03 +0700

    add file

---
+++ new-jdbc-2019/src/main/java/com/laptrinhjavawed/model/AbstractModel.java
@@ -0,0 +1,52 @@
+package com.laptrinhjavawed.model;
+
+import java.sql.Timestamp;
+
+public class AbstractModel {
+	private Long id;
+	private Timestamp createdDate;
+	private Timestamp modifiledDate;
+	private String createdBy;
+	private String modifiedBy;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Timestamp getCreatedDate() {
+		return createdDate;
+	}
+
+	public void setCreatedDate(Timestamp createdDate) {
+		this.createdDate = createdDate;
+	}
+
+	public Timestamp getModifiledDate() {
+		return modifiledDate;
+	}
+
+	public void setModifiledDate(Timestamp modifiledDate) {
+		this.modifiledDate = modifiledDate;
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+
+	public String getModifiedBy() {
+		return modifiedBy;
+	}
+
+	public void setModifiedBy(String modifiedBy) {
+		this.modifiedBy = modifiedBy;
+	}
+
+}
