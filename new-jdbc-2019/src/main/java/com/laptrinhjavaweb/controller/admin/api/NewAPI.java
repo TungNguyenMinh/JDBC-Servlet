641d1c409c6825234d2821119192aa5609201db1
=lethigam150594 <=tungnguyenminh271294@gmail.com>
2019-10-18 17:24:52 +0700

    add

--- new-jdbc-2019/src/main/java/com/laptrinhjavaweb/controller/admin/api/NewAPI.java
+++ new-jdbc-2019/src/main/java/com/laptrinhjavaweb/controller/admin/api/NewAPI.java
@@ -11,10 +11,12 @@ import javax.servlet.http.HttpServletResponse;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.laptrinhjavaweb.model.NewModel;
+import com.laptrinhjavaweb.model.UserModel;
 import com.laptrinhjavaweb.service.INewService;
 import com.laptrinhjavaweb.utils.HttpUtil;
+import com.laptrinhjavaweb.utils.SessionUtil;
 
-@WebServlet(urlPatterns = {"/api-admin-new"})
+@WebServlet(urlPatterns = { "/api-admin-new" })
 public class NewAPI extends HttpServlet {
 
 	@Inject
@@ -28,18 +30,22 @@ public class NewAPI extends HttpServlet {
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("application/json");
 		NewModel newModel = HttpUtil.of(request.getReader()).toModel(NewModel.class);
+		newModel.setCreatedBy(((UserModel) SessionUtil.getInstance().getValue(request, "USERMODEL")).getUserName());
 		newModel = newService.save(newModel);
 		mapper.writeValue(response.getOutputStream(), newModel);
 	}
+
 	@Override
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		ObjectMapper mapper = new ObjectMapper();
 		request.setCharacterEncoding("UTF-8");
 		response.setContentType("application/json");
 		NewModel updateNew = HttpUtil.of(request.getReader()).toModel(NewModel.class);
+		updateNew.setModifiedBy(((UserModel) SessionUtil.getInstance().getValue(request, "USERMODEL")).getUserName());
 		updateNew = newService.update(updateNew);
 		mapper.writeValue(response.getOutputStream(), updateNew);
 	}
+
 	@Override
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		ObjectMapper mapper = new ObjectMapper();
